429d480cfa1480b68558a26373462c35342a6619
antncy <an2002lsth@gmail.com>
2025-10-20 19:04:53 +0000

    fastapi implemented

--- src/main.py
+++ src/main.py
@@ -1,27 +1,31 @@
 import os
-from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
+
+from openai import AsyncOpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model_settings = {
-    "model":os.getenv("MODEL_NAME"),
-    "base_url":os.environ.get("BASE_URL"),
-    "api_key":os.environ.get("API_KEY"),
+    "model":os.environ.get("MODEL_NAME"),
     "temperature": 0.2,
     "timeout": 60,
     "max_tokens": 1024
 }
 
+
+
 async def llm_retrieve(prompt: str, history=[], stream=False) -> str:
 
-    llm = ChatOpenAI(**model_settings).with_retry(stop_after_attempt=2)
+    client = AsyncOpenAI(api_key=os.environ.get("API_KEY"), base_url=os.environ.get("BASE_URL"))
+    
     history.append({
         "role": "user",
         "content": prompt
     })
 
-    message = HumanMessage(content=history)
-    response = await llm.ainvoke([message], stream=stream)
-    return response
+    response = await client.chat.completions.create(
+        messages=history,
+        **model_settings,
+        stream=stream
+    )
+    return response.choices[0].message.content
